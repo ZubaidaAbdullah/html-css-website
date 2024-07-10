86adcb94a555fe60083526afdbd898805907fe0a
Zubaida Abdullah <zubaidaabdullah87@gmail.com>
2024-07-11 02:07:34 +0500

    Update the styling

--- main2.js
+++ main2.js
@@ -1,26 +1,22 @@
-
-    $(document).ready(function(){
-        // Activate Carousel
-        $("#carouselExampleControls").carousel();
-        
-        // Enable Carousel Indicators
-        $(".item1").click(function(){
-            $("#carouselExampleControls").carousel(0);
-        });
-        $(".item2").click(function(){
-            $("#carouselExampleControls").carousel(1);
-        });
-        $(".item3").click(function(){
-            $("#carouselExampleControls").carousel(2);
-        });
-        
-        // Enable Carousel Controls
-        $(".left").click(function(){
-            $("#carouselExampleControls").carousel("prev");
-        });
-        $(".right").click(function(){
-            $("#carouselExampleControls").carousel("next");
-        });
+$(document).ready(function () {
+    $('.carousel').carousel({
+        interval: false
     });
+
+    // Manually trigger carousel controls
+    $('.carousel-control-prev').click(function () {
+        $('.carousel').carousel('prev');
+    });
+
+    $('.carousel-control-next').click(function () {
+        $('.carousel').carousel('next');
+    });
+
+    // Manually trigger carousel indicators
+    $('.carousel-indicators li').click(function () {
+        var index = $(this).data('slide-to');
+        $('.carousel').carousel(index);
+    });
+});
 
  
